4ad34f720f8d61a06ff0ad6a6d72cf22cbc8e121
studboii <42826824+studboii@users.noreply.github.com>
2019-09-03 20:31:25 +0530

    Schema Draft
    This is the basic Schema.

---
+++ bmsripoff.sql
@@ -0,0 +1,122 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 03, 2019 at 05:00 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bmsripoff`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `uid` varchar(50) NOT NULL,
+  `Bid` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Saves the bookings for each user';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deets`
+--
+
+CREATE TABLE `deets` (
+  `Name` varchar(50) NOT NULL,
+  `Synop` mediumtext,
+  `PGRating` enum('U','UA','A','S') NOT NULL DEFAULT 'U',
+  `Language` varchar(15) DEFAULT '"English"'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Just contains movie deets. ';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `uid` varchar(50) NOT NULL,
+  `pass` varchar(50) NOT NULL,
+  `Name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='This is the Table with Login details for the user';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `moviet`
+--
+
+CREATE TABLE `moviet` (
+  `Name` varchar(50) NOT NULL,
+  `Screen` varchar(3) NOT NULL,
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Holds Screens and time a movie is running at';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `s1`
+--
+
+CREATE TABLE `s1` (
+  `Sno` int(11) NOT NULL,
+  `bid` varchar(50) NOT NULL,
+  `Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Booked seats for a given show';
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`uid`,`Bid`);
+
+--
+-- Indexes for table `deets`
+--
+ALTER TABLE `deets`
+  ADD PRIMARY KEY (`Name`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- Indexes for table `moviet`
+--
+ALTER TABLE `moviet`
+  ADD PRIMARY KEY (`Screen`,`Time`);
+
+--
+-- Indexes for table `s1`
+--
+ALTER TABLE `s1`
+  ADD PRIMARY KEY (`Sno`,`bid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
