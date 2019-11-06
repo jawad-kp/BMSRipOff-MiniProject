37e98bf3b663929c2ecb3fe19eeab9652ed5ed62
gaurav7712 <54388960+gaurav7712@users.noreply.github.com>
2019-11-06 19:45:06 +0530

    Add files via upload
    sign in box,bg,linked register page

--- login.php
+++ login.php
@@ -12,9 +12,14 @@
   		.error{ color: red; font-size: 16px }
   		.wrapper{ width:350px; padding: 20px }
   	</style>
-  	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <!--	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="l.css">
-  	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+  	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js">
+  		
+  	</script> -->
+  	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="l.css">
+  	<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   	<!-- Move the styling to an external CSS later on ALSO ADD SESSION FOR FUTURE SCREENS -->
 </head>
 
@@ -27,7 +32,6 @@
 	//Move config outside and integrate later
 	// Create connection
 	$conn = new mysqli($servername, $username, $password,$db);//creating a connection object
-
 	// Check connection
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
@@ -35,7 +39,6 @@
 	//echo "Connected successfully <br>";
 	$uiderr=$pswderr=$btmerr=$uid=$pswd="";
 	$boo = true;
-
 	if ($_SERVER["REQUEST_METHOD"] == "POST") 
 	{
 	    if(empty($_POST["uid"]))
@@ -47,7 +50,6 @@
 	    {
 	    	$uid=chngIP($_POST["uid"]);
 	    }//UID
-
 	    if(empty($_POST["pass"]))
 	    {
 	        $pswderr="Please enter a Password";
@@ -57,7 +59,6 @@
 	    {
 	    	$pswd=chngIP($_POST["pass"]);
 	    }//password
-
 	    if($boo)
 	    {
 	    	$qry = $sql = "SELECT `pass` FROM `login` WHERE uid LIKE '".$uid."'";
@@ -76,7 +77,6 @@
 		    		$_SESSION["user"] = $uid;
 		    		header("Location: http://localhost:8080/DBMS/ViewMovie.php");
 		    	}
-
 		    	else
 		    	{
 		    		$btmerr = "Incorrect User or Pass. Access Declined";
@@ -88,24 +88,22 @@
 		    	$btmerr = "User or pass is incorrect. Outer else triggered";
 		    	$boo = false;
 		    }
-
 	    }
 	}
-
 	function chngIP($data)
         {
             $data = trim($data);
             $data = stripslashes($data);
             $data = htmlspecialchars($data);
             return $data;
-
         }//Refer Register.php for documentation same modify input and delete any space stuff
         ?>
         <div class="container">
         <center>
         <h1 style="color: white">Sign in</h1></center><br>
         <div class="d-flex justify-content-center">
-        			
+        <div class="whi">
+
         <form name="HenloFrens" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method = "post" >
 	        
 				<input class="form-control" type="text" name="uid" placeholder="User ID" size="50" style="width: 300px; height: 30px; border-radius: 5px;">
@@ -118,9 +116,10 @@
 	         <br>
 	         <span class="error"> <?php echo $btmerr;?> </span>	
 	       <br>
-	       <center><button class="btn " style="color: white; font-size: 20px; border: 1.5px solid white; border-radius: 15%" type="submit">Sign in</button></center>
+	        <center><button class="btn " style="color: red; font-size: 20px; border: 1.5px solid red; border-radius: 15%" type="submit">Sign in</button></center>
 	       <br>
-	   </form>
+	       <a class="butt" href="http://localhost:8080/DBMS/register.php">Click Here to Sign up</a>
+	   </form></div>
  		</div>
  	</div>
  <!-- php ends there hopefully -->
