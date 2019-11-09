00a1f2c246e488272320dd61722e6287e49152f7
gaurav7712 <54388960+gaurav7712@users.noreply.github.com>
2019-11-09 13:19:01 +0530

    Add movie css and nav bar

--- AddMovie.php
+++ AddMovie.php
@@ -15,6 +15,11 @@ if (!(isset($_SESSION["adm"]))) {
   	<style type="text/css">
   		.error{ color: red; font-size: 16px }
   	</style>
+  	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="l2.css">
+  <link rel="stylesheet" type="text/css" href="Hover-master/css/hover.css">
+  <link rel="stylesheet" type="text/css" href="CSS Animations/animate.css">
+  	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<?php
@@ -23,16 +28,13 @@ if (!(isset($_SESSION["adm"]))) {
 	$username = "root";
 	$db = "bmsripoff";
 	$password = "";
-
 	// Create connection
 	$conn = new mysqli($servername, $username, $password,$db);//creating a connection object
-
 	// Check connection
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
 	//echo "Connected successfully <br>";
-
 	$mverr=$synerr=$raterr=$langerr="";
     $mvnm=$synopsis=$rating=$lang="aa";
     $boo= true; //this is our flag to make sure registration is legit
@@ -47,8 +49,6 @@ if (!(isset($_SESSION["adm"]))) {
 	    {
 	    	$mvnm=chngIP($_POST["mvnm"]);
 	    }//UID
-
-
 	     if(empty($_POST["synopsis"]))
 	    {
 	        $synerr="No Synopsis input";
@@ -58,8 +58,6 @@ if (!(isset($_SESSION["adm"]))) {
 	    {
 	    	$synopsis=chngIP($_POST["synopsis"]);
 	    }//name
-
-
 	    if(empty($_POST["rating"]))
 	    {
 	        $raterr="No Certification Selected";
@@ -69,7 +67,6 @@ if (!(isset($_SESSION["adm"]))) {
 	    {
 	    	$rating=chngIP($_POST["rating"]);
 	    }//Password
-
 	    if(empty($_POST["lang"]))
 	    {
 	        $langerr="No Language Selected";
@@ -82,22 +79,17 @@ if (!(isset($_SESSION["adm"]))) {
 	    send_data($boo,$mvnm,$synopsis,$rating, $lang);//passing the globals because otherwise it's a pain in the ass
 	    
 	}//if for emptyData ends here
-
 	/* By the end of this we've verified data and have sent it to our sending function. I'll see how we can make this smaller once we get further along. For now it works and I'll find ways to imporve efficiency if time permits*/
-
-
 	function chngIP($data)
         {
             $data = trim($data);
             $data = stripslashes($data);
             $data = htmlspecialchars($data);
             return $data;
-
         }//this removes trailing spaces and makes it an html element so you can't rip it off don't know why stripslashes but meh but it sorta removes the special characters if the user adds any.
         function send_data($bleh,$MovieName,$Synop,$PGRating, $Language)
         { 
         	echo "Entered send_data with value: ".$bleh."<br>"."Movie's Name".$MovieName."<br>"."Synopsis: ".$Synop."<br>"."Rating: ".$rating."<br>"."Language: ".$Language."<br>";
-
 	    	if($bleh)
 	    	{
 		    	$qry = "INSERT INTO `deets` (`Name`, `Synop`, `PGRating`, `Language`) VALUES (?,?,?,?)";
@@ -116,22 +108,54 @@ if (!(isset($_SESSION["adm"]))) {
 		    		$GLOBALS['mverr'] = "Movie has already been added. Please Use a different name";
 		    	$GLOBALS['conn']->close();
 			}
-
     	}//send_data
 	//echo "hello";
     ?> 
+    <ul>
+  <li><a class="active" href="">ADD MOVIE</a></li>
+  <li><a href="default.asp">ADD SHOW</a></li>
+  <li><a href="news.asp">DELETE MOVIE</a></li>
+  <li><a href="contact.asp">DELETE SHOW</a></li>
+
+</ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="container-fluid">
+    <center><h1 style="color: white; font-size: 40px;" class="fadeInDownBig animated" >Add Movie</h1></center><br><br><br>
+    <div class="d-flex justify-content-center">
+
+    	<div class="pos">
 	 <form name="HenloFrens" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method = "post" > 
-		<label > Movie's Name </label>
+		<div class="row form-group">
+	 		<div class="col-sm-4">
+				<label > Movie's Name </label></div>
+				<div class="col-sm-6">
 		<input type="text" name="mvnm" placeholder="Movie's Name" style="width: 300px; height: 30px; border-radius: 5px;">
 		<span class="error">* <?php echo $mverr;?></span>	
-		<br>
-
-		<label >Synopsis</label>
+		</div></div>
+		<div class="row form-group">
+	 		<div class="col-sm-4">
+				<label class="pos">Synopsis</label></div>
+				<div class="col-sm-6">
 		<textarea rows="4" cols="50" name="synopsis"></textarea>
 		<span class="error">*  <?php echo $synerr;?> </span>	
-		<br>
+		</div></div>
 
-		<label>Rating</label>
+		<div class="row form-group">
+	 		<div class="col-sm-4">
+				<label>Rating</label></div>
+		<div class="col-sm-6">
            <select name="rating">
            		<option value="U">U</option>
            		<option value="UA">UA</option>
@@ -139,16 +163,22 @@ if (!(isset($_SESSION["adm"]))) {
            		<option value="S">S</option>
            </select>
            <span class="error">* <?php echo $raterr;?> </span>	
-         <br>	
-
-         <label>Language</label>
+         </div>	</div>
+         
+         <div class="row form-group">
+	 		<div class="col-sm-4">
+         <label>Language</label></div>
+         <div class="col-sm-6">
            <input type="text" placeholder="Language" name="lang">
            <span class="error">* <?php echo $langerr;?> </span>	
-         <br>
+         </div></div>
+         
 
-         <button type="submit">Submit</button>	
+         <center><button class="btn btn-primary hvr-grow" type="submit">Add</button></center>
+			       <br>
+			       
 
 	</form>
-
+</div></div></div>
 </body>
 </html>
