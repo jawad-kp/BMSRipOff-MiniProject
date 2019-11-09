f9f925d31255f0548bc42c8a08e650fe8fbad1f3
gaurav7712 <54388960+gaurav7712@users.noreply.github.com>
2019-11-09 14:08:04 +0530

    register and logout links added to nav bar

--- AddMovie.php
+++ AddMovie.php
@@ -116,6 +116,9 @@ if (!(isset($_SESSION["adm"]))) {
   <li><a href="addshow.php">ADD SHOW</a></li>
   <li><a href="deletemovie.php">DELETE MOVIE</a></li>
   <li><a href="deleteshow.php">DELETE SHOW</a></li>
+  <li><a href="admin_reg.php">REGISTER ADMIN</a></li>
+  <li><a href="">LOGOUT</a></li>
+
 
 </ul>
 
